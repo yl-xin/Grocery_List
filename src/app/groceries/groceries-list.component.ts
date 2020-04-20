088402e94671bf48906423e63ffed4dc55ea51d4
Yanlian Xin <xinyanlian@outlook.com>
2020-04-20 10:25:32 -0400

    get data from service

--- src/app/groceries/groceries-list.component.ts
+++ src/app/groceries/groceries-list.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {IGrocery} from './grocery';
+import { GroceryService } from './grocery.service';
 @Component({
     selector:'app-groceries',
     templateUrl:'groceries-list.component.html',
@@ -9,35 +10,13 @@ export class GroceryListComponent {
     pageTitle:'Component list';
     imageWidth:number=50;
     imageMargin:number=2;
-    groceries:IGrocery[]=[
-        {   
-            "groceryID": 1, 
-            "groceryName": "Brocolli",
-            "groceryCode": "BRC-024",
-            "quantity": 2,
-            "price": 5.99,
-            "gotIt": true,
-            "imageUrl": "assets/images/broccoli.png"    
-        },
-        {
-            "groceryID": 2, 
-            "groceryName": "Slice of Cake",
-            "groceryCode": "SLC-001",
-            "quantity": 6,
-            "price": 1.99,
-            "gotIt": false,
-            "imageUrl": "assets/images/cake-slice.png"    
-        },
-        {
-            "groceryID": 3, 
-            "groceryName": "Watermelon",
-            "groceryCode": "WTM-102",
-            "quantity": 1,
-            "price": 8.99,
-            "gotIt": false,
-            "imageUrl": "assets/images/watermelon.png"   
-        }
-    ];
+    groceries:IGrocery[]=[];
+    // inject the service
+    constructor(private groceryService:GroceryService){}
+    // get data from service using ngOnInit()
+    ngOnInit():void{
+        this.groceries =this.groceryService.getGroceries();
+    }
     showImage:boolean =false;
     toggleImage(): void{
         this.showImage=!this.showImage;
